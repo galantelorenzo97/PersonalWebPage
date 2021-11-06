5dc3420e3393bc3b9dfedfa81e5433c7926f54c3
Engr. Lorenzo Galante <galantelorenzo97@gmail.com>
2021-11-05 22:24:45 -0400

    Fonts and Color stylesheets made global

--- pwp-client/src/index.tsx
+++ pwp-client/src/index.tsx
@@ -1,11 +1,15 @@
+//Stylesheets
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/index.css';
+import './styles/fonts.css';
+import './styles/colors.css';
+
+//Components
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import CustomNavbar from './CustomNavbar';
-import './styles/fonts.css';
 
 ReactDOM.render(
   <React.StrictMode>
